87e3c35052984afc7059b7c6e182f91263bdc8e0
lirs-exe <printf.gamer.tag@gmail.com>
2026-03-19 21:11:49 +0800

    Add files via upload

---
+++ CCDSTRU-13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int i;
+	
+	int C[3] = {1, 2, 3}; 
+	int N[17] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+	int M[9];
+	int R[9], B[9], S[9], T[9];
+	int V[2] = {1, 0};
+	int val[17] = {0};
+	
+	int good = 0, found = 0;
+	int go = 1;
+	int start = 1, ;
+	
+
+	
+	return 0;
+}
