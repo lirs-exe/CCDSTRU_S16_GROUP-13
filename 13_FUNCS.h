56f2b11e14131e15b3d844cd36620eb09271b444
lirs-exe <printf.gamer.tag@gmail.com>
2026-03-25 14:58:44 +0800

    heads up !

--- 13_FUNCS.h
+++ 13_FUNCS.h
@@ -1,3 +1,5 @@
+// HEADS UP: will be coding this on my own IDE to see if the outputs are right, I'll update this file when I finish a part !
+
 void playedEndCard()
 {
 	printf("\n=======================================\n");
