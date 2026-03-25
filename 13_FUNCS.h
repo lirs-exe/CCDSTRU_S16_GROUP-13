1eb9d3178989838476c9ca06abd1640f404ed298
lirs-exe <printf.gamer.tag@gmail.com>
2026-03-25 14:52:23 +0800

    For neatness

---
+++ 13_FUNCS.h
@@ -0,0 +1,22 @@
+void playedExitCard()
+{
+	printf("\n=======================================\n");
+	printf("%s Won! Thank you for Playing!\n", );
+	printf("=======================================\n");
+
+}
+
+void GameProper()
+{
+	printf("");
+	
+	if (over)
+		{playedExitCard()};
+}
+
+void ExitCard()
+{
+	printf("\n=======================================\n");
+	printf("	Thank you for Playing!\n");
+	printf("=======================================\n");
+}
